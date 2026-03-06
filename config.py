c25fbe3456028e741ceeb9ad2bad7e9ceae51dcf
khomichkovamaria <131908919+khomichkovamaria@users.noreply.github.com>
2026-03-06 15:02:21 +0400

    Update config.py
    фикс конфига

--- config.py
+++ config.py
@@ -5,15 +5,8 @@ CG_API_KEY = os.getenv('CG_API_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DATABASE_URL:
-    # 1. Исправляем протокол на асинхронный
+    # Просто меняем протокол на асинхронный
     if DATABASE_URL.startswith("postgresql://"):
         DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
-    
-    # 2. Полностью очищаем URL от старых параметров (всего, что после ?)
-    base_url = DATABASE_URL.split("?")[0]
-
-    # 3. Добавляем параметры, которые ГАРАНТИРОВАННО отключают кэш запросов
-    # Это решает проблему "prepared statement already exists"
-    DATABASE_URL = f"{base_url}?prepared_statement_cache_size=0&statement_cache_size=0"
 
 PORT = int(os.getenv("PORT", 8080))
