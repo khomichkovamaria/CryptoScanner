0dbe00ce207ba3ec50b3e63eaf05bee76277c08f
khomichkovamaria <131908919+khomichkovamaria@users.noreply.github.com>
2026-03-06 13:07:54 +0400

    Update config.py
    фикс конфига

--- config.py
+++ config.py
@@ -5,8 +5,15 @@ API_TOKEN = os.getenv('API_TOKEN')
 CG_API_KEY = os.getenv('CG_API_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-# Если БД подключена, добавляем спец-префикс для асинхронности
-if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+if DATABASE_URL:
+    # 1. Заменяем префикс на асинхронный
+    if DATABASE_URL.startswith("postgresql://"):
+        DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+    
+    # 2. Удаляем параметры pgbouncer, которые вызывают ошибку TypeError
+    if "?pgbouncer=true" in DATABASE_URL:
+        DATABASE_URL = DATABASE_URL.replace("?pgbouncer=true", "")
+    elif "&pgbouncer=true" in DATABASE_URL:
+        DATABASE_URL = DATABASE_URL.replace("&pgbouncer=true", "")
 
 PORT = int(os.getenv("PORT", 8080))
